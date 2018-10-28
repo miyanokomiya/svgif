65f1d7c31195aa40a3d1bbd751ac1f44e35ee640
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-10-29 00:11:30 +0900

    storeファイル統合

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import App from './App'
 import router from './router'
-import store from './store'
+import store from '../main/store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
