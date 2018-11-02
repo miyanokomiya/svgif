a51a9ba9659509206ab57a8ca3476cf53edfce4a
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-11-02 23:04:21 +0900

    actionのテスト追加

--- test/unit/specs/store/modules/clips/actions.spec.js
+++ test/unit/specs/store/modules/clips/actions.spec.js
@@ -31,6 +31,108 @@ function testAction({
 }
 
 describe('store/modules/clips/actions', () => {
+  describe('CREATE_CLIP', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.CREATE_CLIP],
+        payload: { clip: { id: 2 }, index: 1 },
+        mutations: [
+          {
+            type: types.m.ADD_CLIP,
+            test: ({ clip, index }) => {
+              expect(clip.id).to.equal(2)
+              expect(clip.base64).to.equal('')
+              expect(index).to.equal(1)
+            }
+          }
+        ]
+      })
+    })
+  })
+  describe('DELETE_CLIP', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.DELETE_CLIP],
+        payload: 1,
+        mutations: [
+          {
+            type: types.m.REMOVE_CLIP,
+            test: id => {
+              expect(id).to.equal(1)
+            }
+          }
+        ]
+      })
+    })
+  })
+  describe('DELETE_ALL_CLIP', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.DELETE_ALL_CLIP],
+        mutations: [
+          {
+            type: types.m.REMOVE_ALL_CLIP
+          }
+        ]
+      })
+    })
+  })
+  describe('SELECT_CLIP', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.SELECT_CLIP],
+        payload: 1,
+        mutations: [
+          {
+            type: types.m.SELECT_CLIP,
+            test: id => {
+              expect(id).to.equal(1)
+            }
+          }
+        ]
+      })
+    })
+  })
+  describe('SWAP_CLIP_ORDER', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.SWAP_CLIP_ORDER],
+        payload: { from: 1, to: 10 },
+        mutations: [
+          {
+            type: types.m.SWAP_CLIP_ORDER,
+            test: ({ from, to }) => {
+              expect(from).to.equal(1)
+              expect(to).to.equal(10)
+            }
+          }
+        ]
+      })
+    })
+  })
+  describe('UPDATE_DELAY', () => {
+    it('mutationが正しく実行されること', done => {
+      testAction({
+        done,
+        action: actions[types.a.UPDATE_DELAY],
+        payload: { id: 1, delay: 10 },
+        mutations: [
+          {
+            type: types.m.UPDATE_DELAY,
+            test: ({ id, delay }) => {
+              expect(id).to.equal(1)
+              expect(delay).to.equal(10)
+            }
+          }
+        ]
+      })
+    })
+  })
   describe('CLONE_CLIP', () => {
     it('base64 width height が継承されたクリップが追加されること', done => {
       const state = {
