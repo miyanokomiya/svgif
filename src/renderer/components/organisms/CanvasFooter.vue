ab908fb28733e597d2a76aeb7a68368047119f5c
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-11-03 12:24:23 +0900

    clip削除・時間変更機能をフッターに移植

--- src/renderer/components/organisms/CanvasFooter.vue
+++ src/renderer/components/organisms/CanvasFooter.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="canvas-footer">
+    <template v-if="SELECTED_CLIP">
+      <el-button
+        class="delete-button"
+        type="danger" size="mini" round
+        icon="el-icon-delete"
+        @click="deleteClip"
+      />
+      <div>
+        <span>Time(s): </span>
+        <el-input
+          class="delay-input"
+          size="mini"
+          placeholder="Delay"
+          type="number"
+          step="0.1"
+          :value="SELECTED_CLIP.delay / 1000"
+          @input="updateDelay"
+        />
+      </div>
+    </template>
+    <span class="total-delay">Total Time(s): {{WHOLE_DELAY / 1000}}</span>
     <el-button
       type="primary"
       size="mini"
@@ -13,10 +34,51 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import clipTypes from '@main/store/modules/clips/types'
+
 export default {
+  computed: {
+    ...mapGetters({
+      SELECTED_CLIP: clipTypes.g.SELECTED_CLIP,
+      WHOLE_DELAY: clipTypes.g.WHOLE_DELAY
+    })
+  },
   methods: {
+    ...mapActions({
+      _deleteClip: clipTypes.a.DELETE_CLIP,
+      _updateDelay: clipTypes.a.UPDATE_DELAY
+    }),
     createGif() {
       this.$emit('createGif')
+    },
+    updateDelay(val) {
+      const delay = parseFloat(val) * 1000
+      if (delay < 10 || 10000 < delay) {
+        this.$notify.error({
+          title: 'Error',
+          message: 'Delay time must be 0.01 to 10.'
+        })
+      } else {
+        this._updateDelay({ id: this.SELECTED_CLIP.id, delay })
+      }
+    },
+    deleteClip() {
+      this.$confirm(
+        'This will permanently delete the clip. Continue?',
+        'Warning',
+        {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }
+      ).then(() => {
+        this._deleteClip(this.SELECTED_CLIP.id)
+        this.$message({
+          type: 'success',
+          message: 'Delete completed'
+        })
+      })
     }
   }
 }
@@ -30,7 +92,16 @@ export default {
   height: 100%;
   background-color: #b3c0d1;
   color: #333;
-  .create-gif-button {
+  > * {
+    margin-left: 1rem;
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+  .delay-input {
+    width: 10rem;
+  }
+  .total-delay {
     margin-left: auto;
   }
 }
