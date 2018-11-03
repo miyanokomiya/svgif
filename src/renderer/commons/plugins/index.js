6ebf1dc17b109cbc4ba735c00d6f5a330d8717bc
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-11-03 23:07:18 +0900

    windowサイズ取得プラグイン作成

---
+++ src/renderer/commons/plugins/index.js
@@ -0,0 +1,19 @@
+export default {
+  install: Vue => {
+    const state = {
+      windowInfo: {
+        width: 0,
+        height: 0
+      }
+    }
+    const onResize = () => {
+      state.windowInfo = {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+      }
+    }
+    window.addEventListener('resize', onResize)
+    onResize()
+    Vue.util.defineReactive(Vue.prototype, '$svgif', state)
+  }
+}
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,12 +6,14 @@ import router from './router'
 import store from '../main/store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import plugins from './commons/plugins'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(plugins)
 
 /* eslint-disable no-new */
 new Vue({
