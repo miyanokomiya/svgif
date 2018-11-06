85caf281b5a86e8eaa48bf39bc9248621231c27d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2018-11-06 20:35:58 +0900

    geo関数追加

--- src/renderer/commons/utils/geo.js
+++ src/renderer/commons/utils/geo.js
@@ -1,3 +1,7 @@
+// //////
+// 判定系
+// //////
+
 export function isPointInRect(rect, p) {
   if (p.x < rect.x) return false
   if (p.y < rect.y) return false
@@ -20,4 +24,40 @@ export function isRectInRect(outer, inner) {
   )
     return false
   return true
+}
+
+// //////
+// 座標系
+// //////
+
+export function distance(p1, p2) {
+  return norm(vector(p1, p2))
+}
+
+export function norm(v) {
+  return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2))
+}
+
+export function vector(from, to) {
+  return {
+    x: to.x - from.x,
+    y: to.y - from.y
+  }
+}
+
+export function unitVector(from, to) {
+  const v = vector(from, to)
+  const n = norm(v)
+  if (n === 0) return { x: 0, y: 0 }
+  return {
+    x: v.x / n,
+    y: v.y / n
+  }
+}
+
+export function center(p1, p2) {
+  return {
+    x: (p1.x + p2.x) / 2,
+    y: (p1.y + p2.y) / 2
+  }
 }
--- test/unit/specs/commons/utils/geo.spec.js
+++ test/unit/specs/commons/utils/geo.spec.js
@@ -1,5 +1,7 @@
 import * as geo from '@/commons/utils/geo'
 
+const THRESHOLD = 0.001
+
 describe('geo 幾何計算モジュールのテスト', () => {
   describe('isPointInRect 点が矩形に含まれるか判定', () => {
     const rect = { x: 0, y: 0, width: 1, height: 2 }
@@ -60,6 +62,48 @@ describe('geo 幾何計算モジュールのテスト', () => {
         const rect2 = { x: 0.5, y: 1, width: 0.3, height: 1.5 }
         expect(geo.isRectInRect(rect, rect2)).to.equal(false)
       })
+    })
+  })
+  describe('distance ２点の距離取得', () => {
+    it('正しく取得されること', () => {
+      const d = geo.distance({ x: 1, y: 2 }, { x: 4, y: 6 })
+      expect(d).to.closeTo(5, THRESHOLD)
+    })
+  })
+  describe('norm ノルム取得', () => {
+    it('正しく取得されること', () => {
+      const n = geo.norm({ x: 3, y: 4 })
+      expect(n).to.closeTo(5, THRESHOLD)
+    })
+  })
+  describe('vector ２点のベクトル取得', () => {
+    it('正しく取得されること', () => {
+      const v = geo.vector({ x: 1, y: 2 }, { x: 2, y: 4 })
+      expect(v.x).to.equal(1)
+      expect(v.y).to.equal(2)
+    })
+  })
+  describe('unitVector ２点の単位ベクトル取得', () => {
+    context('同座標の場合', () => {
+      it('ゼロベクトルが取得されること', () => {
+        const v = geo.unitVector({ x: 1, y: 2 }, { x: 1, y: 2 })
+        expect(v.x).to.equal(0)
+        expect(v.y).to.equal(0)
+      })
+    })
+    context('同座標ではないの場合', () => {
+      it('正しく取得されること', () => {
+        const v = geo.unitVector({ x: 0, y: 0 }, { x: 3, y: 4 })
+        expect(v.x).to.closeTo(3 / 5, THRESHOLD)
+        expect(v.y).to.closeTo(4 / 5, THRESHOLD)
+      })
+    })
+  })
+  describe('center ２点の中心取得', () => {
+    it('正しく取得されること', () => {
+      const c = geo.center({ x: 0, y: 0 }, { x: 2, y: 4 })
+      expect(c.x).to.equal(1)
+      expect(c.y).to.equal(2)
     })
   })
 })
